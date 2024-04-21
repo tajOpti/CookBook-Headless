fec597a043cebf2ed55cfe1653b53bd082bfd57b
tajOpti <tajbiul.rawol@optimizely.com>
2024-04-21 18:37:18 +0600

    Landing page, edit attributes ordering

--- frontend/src/components/widgets/IntroductionBanner.vue
+++ frontend/src/components/widgets/IntroductionBanner.vue
@@ -1,12 +1,11 @@
 <template>
     <section class="Hero">
         <h1 v-epi-edit="'Title'" v-html="title"></h1>
-        <h5 v-epi-edit="'Subtitle'" v-html="subtitle"></h5>
         <EpiProperty v-show="epiDisableEditing" property-name="Title" />
+        <h5 v-epi-edit="'Subtitle'" v-html="subtitle"></h5>
         <EpiProperty v-show="epiDisableEditing" property-name="Subtitle" />
-        <EpiProperty v-show="epiDisableEditing" property-name="HeroImage" />
         <img class="Hero-image" :src="`${heroimage}`" alt="">
-
+        <EpiProperty v-show="epiDisableEditing" property-name="HeroImage" />
         <!-- <div class="Hero-image" v-if="heroimage" :style="{ 'background-image': 'url(' + heroimage + ')' }">
         </div> -->
     </section>
